0bdbba5ab75d17958a6d717fca868afcdde75bac
Tim Burnham <timburnham73@gmail.com>
2024-07-20 19:22:16 -0400

    Upgrading cli

--- src/app/core/store/account-state.module.ts
+++ src/app/core/store/account-state.module.ts
@@ -2,20 +2,14 @@ import { NgModule } from '@angular/core';
 import { NgxsModule } from '@ngxs/store';
 import { AccountState } from './account.state';
 import { AccountService } from '../services/account.service';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 
 
 
-@NgModule({
-  declarations: [],
-  imports: [
-    NgxsModule.forFeature([AccountState]),
-    NgxsStoragePluginModule.forRoot(),
-    HttpClientModule
-  ],
-  providers: [
-    AccountService
-  ]
-})
+@NgModule({ declarations: [], imports: [NgxsModule.forFeature([AccountState]),
+        NgxsStoragePluginModule.forRoot()], providers: [
+        AccountService,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AccountStateModule { }
